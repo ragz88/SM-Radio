28bc6780c65f1775ae0d2d87daac77b37fd46afd
Erik Prinz <erik88d3@gmail.com>
2019-06-12 13:42:29 +0200

    Added tuning sounds and static. Messed with feel.

--- SM Radio App/Assets/Scripts/BarSwipe.cs
+++ SM Radio App/Assets/Scripts/BarSwipe.cs
@@ -12,6 +12,10 @@ public class BarSwipe : MonoBehaviour
     public bool useMouse = false;
     public bool useRisidualForce = true;
 
+    public AudioSource tuningSound;
+    public float tuningSoundRamp = 0.03f;
+    bool isTuning = false;
+
     Rigidbody2D body;
 
     // Start is called before the first frame update
@@ -31,6 +35,8 @@ public class BarSwipe : MonoBehaviour
                 startPos = new Vector2(Input.GetTouch(0).position.x, 0);
                 barInitPos = transform.position;
                 body.velocity = Vector2.zero;
+
+                //tuningSound.Play();
             }
 
             ///////////////////////////////////////////////////////////////////////////
@@ -41,6 +47,7 @@ public class BarSwipe : MonoBehaviour
 
                 transform.position = barInitPos + (direction/100);
 
+                isTuning = true;
             }
             ///////////////////////////////////////////////////////////////////////////
 
@@ -52,18 +59,20 @@ public class BarSwipe : MonoBehaviour
                 endPos = new Vector2(Input.GetTouch(0).position.x, 0);
                 direction = endPos - startPos;
 
+                isTuning = false;
+
                 if (useRisidualForce)
                 {
 
-                    body.drag = touchTimeInterval * 10;
-                    body.angularDrag = touchTimeInterval * 10;
+                    body.drag = touchTimeInterval * 25;
+                    body.angularDrag = touchTimeInterval * 25;
 
                     /*if (touchTimeInterval * 10 > 10)
                     {
                         body.drag = 10;
                         body.angularDrag = 10;
                     }
-                    else*/ if (touchTimeInterval * 10 < 0.2f)
+                    else*/ if (touchTimeInterval * 25 < 0.2f)
                     {
                         body.drag = 0.2f;
                         body.angularDrag = 0.2f;
@@ -81,12 +90,16 @@ public class BarSwipe : MonoBehaviour
                 startPos = new Vector2(Input.mousePosition.x, 0);
                 barInitPos = transform.position;
                 body.velocity = Vector2.zero;
+
+                ///tuningSound.Play();
             }
 
             if (Input.GetMouseButton(0))
             {
                 endPos = new Vector2(Input.mousePosition.x, 0);
                 direction = endPos - startPos;
+
+                isTuning = true;
 
                 transform.position = barInitPos + (direction/100);
             }
@@ -99,17 +112,19 @@ public class BarSwipe : MonoBehaviour
                 endPos = new Vector2(Input.mousePosition.x, 0);
                 direction = endPos - startPos;
 
+                isTuning = false;
+
                 if (useRisidualForce)
                 {
-                    body.drag = touchTimeInterval * 10;
-                    body.angularDrag = touchTimeInterval * 10;
+                    body.drag = touchTimeInterval * 25;
+                    body.angularDrag = touchTimeInterval * 25;
 
                     /*if (touchTimeInterval * 10 > 10)
                     {
                         body.drag = 10;
                         body.angularDrag = 10;
                     }
-                    else*/ if (touchTimeInterval * 10 < 0.2f)
+                    else*/ if (touchTimeInterval * 25 < 0.2f)
                     {
                         body.drag = 0.2f;
                         body.angularDrag = 0.2f;
@@ -119,5 +134,15 @@ public class BarSwipe : MonoBehaviour
                 }
             }
         }
+
+        if (isTuning && tuningSound.volume < 1)
+        {
+            tuningSound.volume += tuningSoundRamp;
+        }
+        else if (!isTuning && tuningSound.volume > 0)
+        {
+            tuningSound.volume -= tuningSoundRamp;
+        }
+        
     }
 }
---
+++ SM Radio App/Assets/Scripts/Station.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Station : MonoBehaviour
+{
+
+    public string stationName;
+    public Color stationColour;
+    public float stationFreq;
+
+    AudioSource source;
+    AudioSource staticSound;
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        staticSound = gameObject.GetComponentInParent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
